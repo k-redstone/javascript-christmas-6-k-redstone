8e0cbd7b74ddfe50e02d32ed9cbcdacd25261859
k-redstone <che0618@naver.com>
2023-11-14 01:38:56 +0900

    refactor: 로직 변경으로 인한 테스트코드 변경

--- __tests__/priceTest/priceTest.js
+++ __tests__/priceTest/priceTest.js
@@ -1,48 +1,35 @@
 import Bill from "../../src/model/Bill.js";
-import Order from "../../src/model/Order.js";
+
+const testData = [
+  { name: "초코케이크", count: 1, price: 15000, type: "dessert" },
+  { name: "아이스크림", count: 2, price: 5000, type: "dessert" },
+  { name: "티본스테이크", count: 1, price: 55000, type: "mainCourse" },
+];
 
 describe("가격 테스트", () => {
   test("할인 전 총 주문 금액", () => {
-    const testData = [
-      { input: "해산물파스타-2,레드와인-1,초코케이크-1", output: 145000 },
-      {
-        input: "양송이수프-10,해산물파스타-1,초코케이크-1",
-        output: 110000,
-      },
-    ];
+    const output = 80000;
+    const bill = new Bill(testData, 19, 0);
 
-    testData.forEach(({ input, output }) => {
-      const order = new Order(input);
-      expect(order.getBeforeDiscountPrice()).toEqual(output);
-    });
+    expect(bill.getBeforeDiscountPrice()).toEqual(output);
   });
 
   test("크리스마스 할인 금액 성공", () => {
-    const testData = [];
-    const output = 2800;
-
+    const output = -2800;
     const bill = new Bill(testData, 19, 0);
 
     expect(bill.getChristmasDiscountPrice()).toEqual(output);
   });
 
   test("크리스마스 할인 금액 실패", () => {
-    const testData = [];
     const output = 0;
-
     const bill = new Bill(testData, 26, 0);
 
     expect(bill.getChristmasDiscountPrice()).toEqual(output);
   });
 
   test("평일 할인금액", () => {
-    const testData = [
-      { name: "초코케이크", count: 1, price: 15000, type: "dessert" },
-      { name: "아이스크림", count: 2, price: 5000, type: "dessert" },
-      { name: "티본스테이크", count: 1, price: 55000, type: "mainCourse" },
-    ];
-    const output = 6069;
-
+    const output = -6069;
     const bill = new Bill(testData, 17, 0);
 
     expect(bill.getWeekDiscountPrice()).toEqual(output);
@@ -55,7 +42,7 @@ describe("가격 테스트", () => {
       { name: "티본스테이크", count: 1, price: 55000, type: "mainCourse" },
       { name: "해산물파스타", count: 2, price: 55000, type: "mainCourse" },
     ];
-    const output = 6069;
+    const output = -6069;
 
     const bill = new Bill(testData, 16, 5, false);
 
@@ -63,9 +50,7 @@ describe("가격 테스트", () => {
   });
 
   test("특별 할인금액", () => {
-    const testData = [];
-    const output = 1000;
-
+    const output = -1000;
     const bill = new Bill(testData, 31, 0);
 
     expect(bill.getSpecialDiscountPrice()).toEqual(output);
@@ -76,7 +61,7 @@ describe("가격 테스트", () => {
       { name: "티본스테이크", count: 1, price: 55000, type: "mainCourse" },
       { name: "해산물파스타", count: 2, price: 55000, type: "mainCourse" },
     ];
-    const output = 25000;
+    const output = -25000;
 
     const bill = new Bill(testData, 31, 0, true);
 
